5f0d57d936804807580382cbe1f892b6315a5f4b
Andreia Bonomi DECSKILL <andreiabonomi.DECSKILL@edp.com>
2023-02-06 15:18:17 +0000

    description

--- techsial_app/lib/main.dart
+++ techsial_app/lib/main.dart
@@ -32,7 +32,42 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<dynamic> listOfArrays = [];
+  Map listOfArrays = {
+    "status": "ok",
+    "totalResults": 5892,
+    "articles": [
+      {
+        "source": {"id": null, "name": "Forbes"},
+        "author":
+            "Robin Gagnon, Forbes Councils Member, \n Robin Gagnon, Forbes Councils Member\n https://www.forbes.com/sites/forbesbusinesscouncil/people/robingagnon/",
+        "title": "Comparing Franchise Vs. Independent Restaurants For 2023",
+        "description":
+            "Based on several key factors, will franchise or independent restaurants be better positioned in 2023? Here is a comparison.",
+        "url":
+            "https://www.forbes.com/sites/forbesbusinesscouncil/2023/02/06/comparing-franchise-vs-independent-restaurants-for-2023/",
+        "urlToImage":
+            "https://imageio.forbes.com/specials-images/imageserve/63dd1d4518a8e0add39afec6/0x0.jpg?format=jpg&width=1200",
+        "publishedAt": "2023-02-06T13:30:00Z",
+        "content":
+            "Robin Gagnon is the CEO and Co-Founder of We Sell Restaurants, a business broker franchise focused on the sale of restaurants.\r\ngetty\r\nRestaurant industry sales are expected to reach 855 billion in … [+6557 chars]"
+      },
+      {
+        "source": {"id": null, "name": "Yahoo Entertainment"},
+        "author": "Future Market Insights Global and Consulting Pvt. Ltd.",
+        "title":
+            "Vodka Market Expected to Hit US 68.25 billion by 2033, & is predicted to occur at a CAGR of 5.5% by 2033 | Future Market Insights, Inc.",
+        "description":
+            "From 2023 to 2033, Asia Pacific Vodka Market is expected to grow at a CAGR of 6.4%. he category for flavored vodka is likely to grow more quickly, with a...",
+        "url":
+            "https://finance.yahoo.com/news/vodka-market-expected-hit-us-133000212.html",
+        "urlToImage":
+            "https://s.yimg.com/ny/api/res/1.2/qP4r51hLByRzlY33mD2ISw--/YXBwaWQ9aGlnaGxhbmRlcjt3PTEyMDA7aD03MzA-/https://media.zenfs.com/en/globenewswire.com/433e6507c3cf94b90d194380426a7629",
+        "publishedAt": "2023-02-06T13:30:00Z",
+        "content":
+            "Future Market Insights Global and Consulting Pvt. Ltd.\r\nFrom 2023 to 2033, Asia Pacific Vodka Market is expected to grow at a CAGR of 6.4%. he category for flavored vodka is likely to grow more quick… [+8427 chars]"
+      },
+    ]
+  };
 
   @override
   void initState() {
@@ -53,6 +88,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    var answers = listOfArrays['articles'] as List<Map<String, Object>>;
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -78,7 +115,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     margin: const EdgeInsets.symmetric(
                         horizontal: 16.0, vertical: 10.0),
                     child: ListView(
-                      children: listOfArrays
+                      children: answers
                           .map(
                             (array) => Container(
                               decoration: BoxDecoration(
@@ -88,9 +125,12 @@ class _MyHomePageState extends State<MyHomePage> {
                               child: Column(
                                 mainAxisAlignment:
                                     MainAxisAlignment.spaceEvenly,
-                                children: [
-                                  Image.asset('images/pic1.jpg'),
-                                  Text(array.toString()),
+                                children: <Widget>[
+                                  Image(
+                                    image: NetworkImage(
+                                        answers[0]['urlToImage'] as String),
+                                  ),
+                                  Text(array['description'].toString()),
                                 ],
                               ),
                             ),
