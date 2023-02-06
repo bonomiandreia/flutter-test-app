4ce71c7519add3ea0580a045ad21571ef21bca9d
Andreia Bonomi DECSKILL <andreiabonomi.DECSKILL@edp.com>
2023-02-06 19:35:29 +0000

    test list

--- techsial_app/lib/main.dart
+++ techsial_app/lib/main.dart
@@ -32,42 +32,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Map listOfArrays = {
-    "status": "ok",
-    "totalResults": 5892,
-    "articles": [
-      {
-        "source": {"id": null, "name": "Forbes"},
-        "author":
-            "Robin Gagnon, Forbes Councils Member, \n Robin Gagnon, Forbes Councils Member\n https://www.forbes.com/sites/forbesbusinesscouncil/people/robingagnon/",
-        "c": "Comparing Franchise Vs. Independent Restaurants For 2023",
-        "description":
-            "Based on several key factors, will franchise or independent restaurants be better positioned in 2023? Here is a comparison.",
-        "url":
-            "https://www.forbes.com/sites/forbesbusinesscouncil/2023/02/06/comparing-franchise-vs-independent-restaurants-for-2023/",
-        "urlToImage":
-            "https://imageio.forbes.com/specials-images/imageserve/63dd1d4518a8e0add39afec6/0x0.jpg?format=jpg&width=1200",
-        "publishedAt": "2023-02-06T13:30:00Z",
-        "content":
-            "Robin Gagnon is the CEO and Co-Founder of We Sell Restaurants, a business broker franchise focused on the sale of restaurants.\r\ngetty\r\nRestaurant industry sales are expected to reach 855 billion in … [+6557 chars]"
-      },
-      {
-        "source": {"id": null, "name": "Yahoo Entertainment"},
-        "author": "Future Market Insights Global and Consulting Pvt. Ltd.",
-        "title":
-            "Vodka Market Expected to Hit US 68.25 billion by 2033, & is predicted to occur at a CAGR of 5.5% by 2033 | Future Market Insights, Inc.",
-        "description":
-            "From 2023 to 2033, Asia Pacific Vodka Market is expected to grow at a CAGR of 6.4%. he category for flavored vodka is likely to grow more quickly, with a...",
-        "url":
-            "https://finance.yahoo.com/news/vodka-market-expected-hit-us-133000212.html",
-        "urlToImage":
-            "https://s.yimg.com/ny/api/res/1.2/qP4r51hLByRzlY33mD2ISw--/YXBwaWQ9aGlnaGxhbmRlcjt3PTEyMDA7aD03MzA-/https://media.zenfs.com/en/globenewswire.com/433e6507c3cf94b90d194380426a7629",
-        "publishedAt": "2023-02-06T13:30:00Z",
-        "content":
-            "Future Market Insights Global and Consulting Pvt. Ltd.\r\nFrom 2023 to 2033, Asia Pacific Vodka Market is expected to grow at a CAGR of 6.4%. he category for flavored vodka is likely to grow more quick… [+8427 chars]"
-      },
-    ]
-  };
+  Map listOfArrays = {};
+  var listArticles = [];
 
   @override
   void initState() {
@@ -76,7 +42,8 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void _getDataListNews() async {
-    final response = await http.get(Uri.parse(''));
+    final response = await http.get(Uri.parse(
+        'https://newsapi.org/v2/top-headlines?country=us&category=health&apiKey=a275424604ae48368a0cb5a90f2bdeb7'));
     if (response.statusCode == 200) {
       setState(() {
         listOfArrays = json.decode(response.body);
@@ -88,8 +55,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    var answers = listOfArrays['articles'] as List<Map<String, Object>>;
-
+    listArticles = listOfArrays['articles'];
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -116,35 +82,85 @@ class _MyHomePageState extends State<MyHomePage> {
                       margin: const EdgeInsets.symmetric(
                           horizontal: 20.0, vertical: 16.0),
                       child: ListView(children: [
-                        ...answers.map<Widget>((array) {
-                          var index = answers.indexOf(array);
+                        ...listArticles.map<Widget>((array) {
+                          var index = listArticles.indexOf(array);
                           if (index == 0) {
-                            return Container(
-                                child: Column(
+                            return Row(
                               children: [
-                                Image(
-                                    image: NetworkImage(
-                                        array['urlToImage'].toString())),
-                                Text(array['title'].toString()),
-                                Text(index.toString())
+                                Expanded(
+                                    flex: 1,
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                          color:
+                                              Color.fromARGB(255, 23, 38, 31),
+                                          border: Border(
+                                              bottom: BorderSide(
+                                                  width: 1,
+                                                  color: Colors
+                                                      .greenbrand.shade50))),
+                                      child: Column(
+                                        children: [
+                                          Container(
+                                            child: Image(
+                                              image: NetworkImage(
+                                                  array['urlToImage']
+                                                      .toString()),
+                                              fit: BoxFit.fill,
+                                            ),
+                                          ),
+                                          Container(
+                                            margin: EdgeInsets.all(25.0),
+                                            child: Text(
+                                              array['title'],
+                                              style: TextStyle(
+                                                fontSize: 25,
+                                                fontWeight: FontWeight.bold,
+                                                color: Colors.white,
+                                              ),
+                                            ),
+                                          )
+                                        ],
+                                      ),
+                                    )),
                               ],
-                            ));
+                            );
                           }
                           return Row(
                             children: [
-                              Column(
-                                children: [
-                                  Image(
-                                    image: NetworkImage(
-                                        array['urlToImage'].toString()),
-                                    fit: BoxFit.fill,
-                                    height: 100,
-                                  )
-                                ],
-                              ),
-                              Column(
-                                children: [Text(array['title'].toString())],
-                              )
+                              Expanded(
+                                  flex: 1,
+                                  child: Container(
+                                    padding: const EdgeInsets.all(15),
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceAround,
+                                      children: [
+                                        Image(
+                                          image: NetworkImage(
+                                              array['urlToImage'].toString()),
+                                          fit: BoxFit.fill,
+                                          height: 70,
+                                        ),
+                                      ],
+                                    ),
+                                  )),
+                              Expanded(
+                                  flex: 3,
+                                  child: Container(
+                                    padding: const EdgeInsets.all(10),
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceAround,
+                                      children: [
+                                        Text(
+                                          array['title'],
+                                          style: TextStyle(
+                                              fontSize: 15,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                      ],
+                                    ),
+                                  ))
                             ],
                           );
                         }).toList()
