8868dcf32e40cb8210ebb1c9426e8f99c73ed495
Andreia Bonomi DECSKILL <andreiabonomi.DECSKILL@edp.com>
2023-02-06 10:35:53 +0000

    http dependencies

--- techsial_app/lib/main.dart
+++ techsial_app/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(const MyApp());
@@ -13,16 +15,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-
         primarySwatch: Colors.greenbrand,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -33,28 +25,32 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-List<List<int>> listOfArrays = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-];
-
 class _MyHomePageState extends State<MyHomePage> {
+  List<dynamic> listOfArrays = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _getDataListNews();
+  }
+
+  void _getDataListNews() async {
+    final response = await http.get(Uri.parse(''));
+    if (response.statusCode == 200) {
+      setState(() {
+        listOfArrays = json.decode(response.body);
+      });
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -77,20 +73,10 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                padding: EdgeInsets.all(16.0),
-                child: Text(
-                  'List Numbers',
-                  style: TextStyle(
-                    fontSize: 24.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.greenbrand.shade900,
-                  ),
-                ),
-              ),
               Expanded(
                 child: Container(
-                    margin: const EdgeInsets.symmetric(horizontal: 16.0),
+                    margin: const EdgeInsets.symmetric(
+                        horizontal: 16.0, vertical: 10.0),
                     child: ListView(
                       children: listOfArrays
                           .map(
@@ -99,7 +85,14 @@ class _MyHomePageState extends State<MyHomePage> {
                                   color: Colors.white,
                                   borderRadius: BorderRadius.circular(0)),
                               padding: const EdgeInsets.all(16.0),
-                              child: Text(array.toString()),
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Image.asset('images/pic1.jpg'),
+                                  Text(array.toString()),
+                                ],
+                              ),
                             ),
                           )
                           .toList(),
